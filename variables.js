0ae669ff323802f82a8d3c7e7ecb49f2b6897757
shaktisingh212 <shaktisingh98291@gmail.com>
2023-10-27 14:22:36 +0530

    newfile

---
+++ variables.js
@@ -0,0 +1,81 @@
+// // how to define a variable
+// // var myName ="Madhuvan"
+// // console.log(myName)
+// var myName =10;
+// console.log(10)
+// const myName = "bittu"
+// // console.log(myName)
+// var _abc = 234567
+// // console.log(_abc)
+// var -abc ="qwerty"
+// console.log(-abc)
+// var @abc ="qwerty"
+// console.log(@abc)
+// var #abc ="qwerty"
+// console.log(#abc)
+// var $abc ="qwerty"
+// console.log($abc)
+// var age =10;
+// console.log(age)
+// var age =25;
+// console.log(age)
+// let age =10
+// let age =20
+// console.log(age)
+// const age =10
+// const age =20
+// console.log(age)
+// var age =10
+// age=20
+// console.log(age)
+// let age =10
+// age=20
+// console.log(age)
+// const age=10
+// age=20
+// console.log(age)
+// var a =10
+// console.log(a)
+// {
+//     console.log(a)
+// }
+// console.log(a)
+// let a=10
+// console.log(a)
+// {
+//     console.log(a)
+// }
+// console.log(a)
+// {
+//     var a=10
+//     console.log(a)
+// }
+// console.log(a)
+// {
+//     var a=10
+//     console.log(a)
+// }
+// console.log(a)
+// {
+//     console.log(a)
+// }
+// {
+//     let a=10
+//     console.log(a)
+// }
+// console.log(a)
+// {
+//     console.log(a)
+// }
+// {
+//     let a=20
+//     console.log(a)
+// }
+{
+  const a = 10;
+  console.log(a);
+}
+// console.log(a);
+// {
+//   console.log(a);
+// }
